34d99900ee2114e5441258e6595194c1503b2b4b
Edu <fabriciocespedes16@gmail.com>
2022-10-02 16:14:36 -0600

    buscar categorias por id

--- src/main/java/com/example/demo/service/ICategoryService.java
+++ src/main/java/com/example/demo/service/ICategoryService.java
@@ -7,4 +7,5 @@ import com.example.demo.response.CategoryResponseRest;
 public interface ICategoryService {
 
 	public ResponseEntity<CategoryResponseRest> search();
+	public ResponseEntity<CategoryResponseRest> searchById(Long id);
 }
